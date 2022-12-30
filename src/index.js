80f742d6498e5d523b9fe627138b3b9aeb6ae6f2
Aaliyah Small <Aaliyahsmall96@yahoo.com>
2022-12-29 23:28:35 -0500

    removing unnessary functions in JS

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function newCity(event) {
   let city = document.querySelector("#city-search");
   let cityLocation = `${city.value}`;
   let h1 = document.querySelector("h1");
-  h1.innerHTML = `Currently in <strong>${cityLocation}</strong>`;
+  h1.innerHTML = `<i class="fa-solid fa-house-chimney"></i>Currently in <strong>${cityLocation}</strong>`;
   let apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityLocation}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showTemp);
@@ -32,10 +32,11 @@ function newCity(event) {
 
 function showTemp(response) {
   let temperature = Math.round(response.data.main.temp);
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `<strong>°${temperature}</strong>`;
+  let temperatureElement = document.querySelector("#dummy-temp");
+  temperatureElement.innerHTML = `<strong>°${temperature}</strong>`;
   let h1 = document.querySelector("h1");
-  h1.innerHTML = `Currently in <strong>${response.data.name}</strong>`;
+  h1.innerHTML = `<i class="fa-solid fa-house-chimney"></i>Currently in <strong>${response.data.name}</strong>`;
+  console.log(response.data);
 }
 
 function locationNow(position) {
@@ -44,21 +45,4 @@ function locationNow(position) {
   let apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
   axios.get(url).then(showTemp);
-}
-
-let celcius = document.querySelector("#c-temp");
-celcius.addEventListener("click", cTemp);
-
-function cTemp(event) {
-  event.preventDefault();
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `°25`;
-}
-let ferinheight = document.querySelector("#f-temp");
-ferinheight.addEventListener("click", fTemp);
-
-function fTemp(event) {
-  event.preventDefault();
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `°75`;
 }
